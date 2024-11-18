2b3100027fe5a005e9e09d1d9eab698c4c86b469
Yaro Shm <yshmarov@gmail.com>
2024-11-18 16:10:23 +0100

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,37 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+urls = %w[
+  https://nitrokit.dev/
+  https://rbui.dev/
+  https://ui.darksea.dev/
+  https://rapidrails.cc/
+  https://github.com/lazaronixon/css-zero
+  https://railsui.com/
+  https://github.com/baoagency/polaris_view_components
+  https://zestui.com/
+  https://railsdesigner.com/
+]
+listings = []
+urls.each do |url|
+  listing = Listing.find_or_create_by(url:)
+  listings << listing
+end
+category = Category.find_or_create_by(name: "Rails UI Component libraries")
+category.listings << listings
+
+urls = %w[
+  https://superails.com/
+  https://www.driftingruby.com/
+  https://webcrunch.com/
+  http://railscasts.com/
+]
+listings = []
+urls.each do |url|
+  listing = Listing.find_or_create_by(url:)
+  listings << listing
+end
+category = Category.find_or_create_by(name: "Ruby on Rails Screencasts")
+category.listings << listings
+
+Listing.all.each do |listing|
+  AttachScreenshotJob.perform_later listing
+  HomepageCrawlerJob.perform_later listing
+end
